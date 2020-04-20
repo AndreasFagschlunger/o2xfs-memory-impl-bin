fe5032cb8e8cfc9a67a91423b528b186d96dc84c
AndreasFagschlunger <github@fagschlunger.co.at>
2020-04-20 22:51:35 +0200

    Use o2xfs-common-bin

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,9 +16,23 @@ val javah by tasks.registering(Exec::class) {
     commandLine(javah, "-d", file("src/main/headers"), "-cp", file("../o2xfs-memory-impl/src/main/java"), "at.o2xfs.memory.impl.win32.Win32MemorySystem")
 }
 
+repositories {
+    if((version as String).endsWith("-SNAPSHOT")) {
+        maven {
+            url = uri("https://repo.fagschlunger.co.at/libs-snapshot-local")
+        }
+    }
+    maven {
+        url = uri("https://repo.fagschlunger.co.at/libs-release-local")
+    }
+}
+
 library {
-    targetMachines.add(machines.windows.x86)
-    targetMachines.add(machines.windows.x86_64)
+    targetMachines.set(listOf(machines.windows.x86, machines.windows.x86_64))
+
+    dependencies {
+        implementation("at.o2xfs:o2xfs-common-bin:1.0-SNAPSHOT")
+    }
 }
 
 val libIncludePath = files("$javaHome/include", "$javaHome/include/win32")
